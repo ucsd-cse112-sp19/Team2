88af3807d062e24ad8183ece4705af814343929f
cajen <topher.jensen63@gmail.com>
2019-05-12 00:28:04 -0700

    update url for core-hello-test.js

--- test/core-hello-test.js
+++ test/core-hello-test.js
@@ -1,11 +1,13 @@
 const { By } = require('selenium-webdriver');
 const { expect } = require('chai');
+const path = require('path');
+const url = `file://${path.join(__dirname, 'core-hello-test.html')}`;
 
 describe('core-hello unit tests', () => {
     const driver = global.driver;
 
     it('should test attributes of core-hello', async () => {
-        await driver.get('./web_components/core-hello/core-hello-test.html');
+        await driver.get(url);
         // await driver.get('https://meat-space.org/web_components/core-hello/core-hello-test.html');
         await (shadowRoot = driver.findElement(By.css('core-hello')));
         assert.equal(shadowRoot instanceof ShadowRoot, true);
