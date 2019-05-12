b24d393b83843d1204a893e1b95d0b6adcb2844e
cajen <topher.jensen63@gmail.com>
2019-05-11 22:48:51 -0700

    Testing selenium web driver & shadow dom access

--- test/core-hello-test.js
+++ test/core-hello-test.js
@@ -1,14 +1,14 @@
 const { By } = require('selenium-webdriver');
 const { expect } = require('chai');
 
-describe('DefaultTest', () => {
+describe('core-hello unit tests', () => {
     const driver = global.driver;
 
     it('should go to meat-space.org and check the BrowserStack img alt attribute', async () => {
-        // await driver.get('https://meat-space.org/news.html');
+        await driver.get('file://web_components/core-hello/core-hello-test.html');
         // await driver.get('https://meat-space.org/web_components/core-hello/core-hello-test.html');
-        // await (shadowRoot = driver.findElement(By.css('core-hello')));
-        // assert.equal(shadowRoot instanceof ShadowRoot, true);
+        await (shadowRoot = driver.findElement(By.css('core-hello')));
+        assert.equal(shadowRoot instanceof ShadowRoot, true);
         // const altAttribute = await driver.findElement(By.className('section-img-logo')).getAttribute('alt');
 
         // expect(altAttribute).to.equal('BrowserStack logo');
