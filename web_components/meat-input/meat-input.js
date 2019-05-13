4709261f0f33b3de987cfcd5c0822ece9555a384
Aaron Brown <amb012@ucsd.edu>
2019-05-12 17:31:06 -0700

    Set up meat-input branch with boilerplate code.

---
+++ web_components/meat-input/meat-input.js
@@ -0,0 +1,43 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<style></style>
+`;
+
+/**
+ * meat-boilerplate webcomponent
+ * @customelement meat-boilerplate
+ * @description A boilerplate for starting new webcomponents
+ * @example <meat-boilerplate></meat-boilerplate>
+ * */
+export class MeatBoilerplateElement extends HTMLElement {
+  /**
+   * Create an instance of MeatButtonElement
+   */
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+    this.shadow.appendChild(template.content.cloneNode(true));
+  }
+
+  /**
+   * Live-cycle method called when the custom element is loaded, often used for initialization
+   */
+  connectedCallback() {}
+
+  /**
+   * Tell the webcomponent to observe these attributes, if any of them are added, changed, or removed, then
+   * call attributeChangedCallback(name, oldVal, newVal)
+   * */
+  static get observedAttributes() {
+    return [];
+  }
+
+  /*
+   * Called whenever one of the attributes specified in observedAttributes() is changed
+   * @param {string} name
+   * @param {string} oldVal
+   * @param {string} newVal
+   * */
+  attributeChangedCallback(name, oldVal, newVal) {}
+}
+window.customElements.define("meat-boilerplate", MeatBoilerplateElement);
