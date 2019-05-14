94abb7602eb3514fb01fcbc336848d55c3fda0c2
Veronica Lin <velin@ucsd.edu>
2019-05-14 14:20:12 -0700

    setup cross-browser testing using testcafe.

---
+++ test/testcafe-hello-core.js
@@ -0,0 +1,29 @@
+import { Selector } from 'testcafe'; // first import testcafe selectors
+
+fixture `core-hello`// declare the fixture
+    .page `https://meat-space.org/web_components/core-hello/core-hello-test.html`;  // specify the start page
+
+
+test("Adds 'Hello world,' to string", async t => {
+    const shadowInput = await Selector(() => document.querySelector('core-hello').shadowRoot.querySelector('#main-text'));
+    const userInput = await Selector(() => document.querySelector('core-hello'));
+    const expectedOutput = shadowInput.textContent + userInput.textContent;
+    await t
+        .expect(shadowInput.innerText).eql('Hello world, ');
+});
+
+test("Adds 'Hello world,' to {input}", async t => {
+    const shadowInput = await Selector(() => document.querySelector('core-hello').shadowRoot.querySelector('#main-text'));
+    const userInput = await Selector(() => document.querySelector('core-hello'));
+
+    await t
+        .expect(shadowInput.innerText).eql("Hello world, ")
+        .expect(userInput.innerText).eql('Meatspace!');
+});
+
+test("Check that tag-name is core-hello", async t => {
+    const component = await Selector(() => document.querySelector('core-hello'));
+
+    await t
+        .expect(component.tagName).eql("core-hello");
+});
