cb349180b3344588be3c95fda72ceea1f9ecdc1d
cajen <topher.jensen63@gmail.com>
2019-05-16 15:18:22 -0700

    Update pipe.sh script to be more user friendly. Usage: npm run pipe <optional argument: nd> . User no longer needs to add branch name in as an argument, they will be promped to enter it at the command line instead. Optional "nd" argument bypasses doc generation when used.

--- scripts/pipe.sh
+++ scripts/pipe.sh
@@ -1,7 +1,36 @@
 #!/usr/bin/env bash
-npm run-script lint
-npm run-script generate-jsdocs
-git add -A
-git commit
-git pull origin $1
-git push origin $1
+read -p "branch name? " branchName
+git branch | grep -w $branchName
+
+if [ $? != 0 ]; then
+echo "no local git branch named $branchName ... exiting ..."
+exit 
+fi
+
+echo "WARNING! will 'git pull' from $branchName & 'git push' to $branchName"
+read -p "continue? (y or n)? " ans
+
+ans=$(echo "$ans" | tr '[:upper:]' '[:lower:]') 
+
+if [ $ans == 'n' ]; then
+  exit 
+else 
+  echo "running linter ..."
+  npm run-script lint
+  if [ $1 == 'nd' ]; then 
+    echo "no docs generated"
+  else
+    echo "running document generator ..."
+    npm run-script generate-jsdocs
+  fi
+  echo "adding files to staging area ..."
+  git add -A
+  echo "commiting changes ..."
+  git commit
+  echo "pulling from $branchName ..."
+  git pull origin $branchName
+  echo "pushing to $branchName ..."
+  git push origin $branchName
+fi
+echo "success!"
+exit 
