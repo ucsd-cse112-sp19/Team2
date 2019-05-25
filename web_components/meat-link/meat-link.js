76e6cf38c1a68daaed6cfe425716fae6afc4620e
Aaron Brown <amb012@ucsd.edu>
2019-05-24 19:17:50 -0700

    Basic functionality implemented.

--- web_components/meat-link/meat-link.js
+++ web_components/meat-link/meat-link.js
@@ -1,35 +1,47 @@
 const template = document.createElement("template");
 template.innerHTML = `
 <style></style>
+<link rel="stylesheet" href="/web_components/meat-link/meat-link.css"/>
+<a id="a" type="reset"></a>
 `;
 
 /**
- * meat-boilerplate webcomponent
- * @customelement meat-boilerplate
- * @description A boilerplate for starting new webcomponents
- * @example <meat-boilerplate></meat-boilerplate>
+ * meat-link webcomponent
+ * @customelement meat-link
+ * @description displays a stylized link
+ * @example <meat-link></meat-link>
  */
-export class MeatBoilerplateElement extends HTMLElement {
+export class MeatLinkElement extends HTMLElement {
   /**
-   * Create an instance of MeatButtonElement
+   * Create an instance of MeatLinkElement
    */
   constructor() {
     super();
     this.shadow = this.attachShadow({ mode: "open" });
     this.shadow.appendChild(template.content.cloneNode(true));
+    this.link = this.shadow.querySelector("#a");
   }
 
   /**
    * Live-cycle method called when the custom element is loaded, often used for initialization
    */
-  connectedCallback() {}
+  connectedCallback() {
+    this.link.textContent = this.textContent;
+  }
 
   /**
    * Tell the webcomponent to observe these attributes, if any of them are added, changed, or removed, then
    * call attributeChangedCallback(name, oldVal, newVal)
    */
   static get observedAttributes() {
-    return [];
+    return [
+      "type",
+      "href",
+      "disabled",
+      "underline",
+      "icon",
+      "bootstrap"
+    ];
   }
 
   /**
@@ -38,6 +50,22 @@ export class MeatBoilerplateElement extends HTMLElement {
    * @param {string} oldVal
    * @param {string} newVal
    */
-  attributeChangedCallback(name, oldVal, newVal) {}
+  attributeChangedCallback(name, oldVal, newVal) {
+    switch (name) {
+      case "disabled":
+        if (newVal == "") this.link.disabled = true;
+        else this.link.disabled = false;
+        break;
+      case "href":
+        this.link.href = newVal;
+        break;
+      case "type":
+        this.link.type = newVal;
+        break;
+      case "bootstrap":
+        this.link.className = newVal;
+        break;
+    }
+  }
 }
-window.customElements.define("meat-boilerplate", MeatBoilerplateElement);
+window.customElements.define("meat-link", MeatLinkElement);
