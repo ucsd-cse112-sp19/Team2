a93afb11d0db277d7fee735e146be334bbb979cc
Alex Monji <ammonji@ucsd.edu>
2019-05-28 00:23:11 -0700

    Added meat-card, still needs bootstrap support

--- web_components/meat-card/meat-card.js
+++ web_components/meat-card/meat-card.js
@@ -2,6 +2,10 @@ const template = document.createElement("template");
 template.innerHTML = `
 <style></style>
 <link rel="stylesheet" href="/web_components/meat-card/meat-card.css"/>
+
+  <slot id="header" name="header"></slot>
+
+<slot id="body" name="body"></slot>
 `;
 
 /**
@@ -16,8 +20,8 @@ export class MeatCardElement extends HTMLElement {
    */
   constructor() {
     super();
-    this.shadow = this.attachShadow({ mode: "open" });
-    this.shadow.appendChild(template.content.cloneNode(true));
+    this.shadowDOM = this.attachShadow({ mode: "open" });
+    this.shadowDOM.appendChild(template.content.cloneNode(true));
   }
 
   /**
@@ -30,7 +34,7 @@ export class MeatCardElement extends HTMLElement {
    * call attributeChangedCallback(name, oldVal, newVal)
    */
   static get observedAttributes() {
-    return [];
+    return ["shadow"];
   }
 
   /**
@@ -39,6 +43,26 @@ export class MeatCardElement extends HTMLElement {
    * @param {string} oldVal
    * @param {string} newVal
    */
-  attributeChangedCallback(name, oldVal, newVal) {}
+  attributeChangedCallback(name, oldVal, newVal) {
+    switch (name) {
+      case "shadow":
+        break;
+    }
+  }
+
+  /**
+   * Getters and Setters
+   */
+  get shadow() {
+    return this.getAttribute("shadow");
+  }
+
+  set shadow(val) {
+    if (val) {
+      this.setAttribute("shadow", val);
+    } else {
+      this.removeAttribute("shadow");
+    }
+  }
 }
 window.customElements.define("meat-card", MeatCardElement);
