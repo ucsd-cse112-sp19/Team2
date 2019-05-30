647c0a9ec842b4f28e546425ee99440187a54742
cajen <topher.jensen63@gmail.com>
2019-05-30 13:05:06 -0700

    update test coverage

--- test/runner.js
+++ test/runner.js
@@ -9,7 +9,7 @@ createTestCafe('localhost', 1337, 1338)
         return runner
             .src(['test/testcafe-hello-core.js', 'test/testcafe-meat-button.js'])
             .browsers(['browserstack:Chrome@74.0:Windows 10', 'browserstack:firefox@66.0:OS X Mavericks']) 
-            .reporter('spec')
+            .reporter('slack')
             .run();
     })
     .then(failedCount => {
