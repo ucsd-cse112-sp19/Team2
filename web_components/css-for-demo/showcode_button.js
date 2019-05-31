598000a83d18d7e005f2d7786dcac2b7cab65c56
XinLiu <xil566@ucsd.edu>
2019-05-31 16:46:06 -0700

    run eslint

--- web_components/css-for-demo/showcode_button.js
+++ web_components/css-for-demo/showcode_button.js
@@ -1,19 +1,19 @@
-var coll = document.getElementsByClassName("collapsible");
-            var i;
-    
-            for (i = 0; i < coll.length; i++) {
-            coll[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                var content = this.nextElementSibling;
-                if (content.style.maxHeight){
-                content.style.maxHeight = null;
-                } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-                } 
-                if (this.innerHTML === "Show Code") {
-                this.innerHTML = "Hide Code";
-                } else {
-                this.innerHTML = "Show Code";
-                }
-            });
-            }
+const coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+    if (this.innerHTML === "Show Code") {
+      this.innerHTML = "Hide Code";
+    } else {
+      this.innerHTML = "Show Code";
+    }
+  });
+}
