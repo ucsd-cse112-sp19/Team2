3b85fe096fafa1c8b9d93c0c0c39439226612825
XinLiu <xil566@ucsd.edu>
2019-05-31 16:41:10 -0700

    added css for demo

---
+++ web_components/css-for-demo/showcode_button.js
@@ -0,0 +1,19 @@
+var coll = document.getElementsByClassName("collapsible");
+            var i;
+    
+            for (i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.maxHeight){
+                content.style.maxHeight = null;
+                } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+                } 
+                if (this.innerHTML === "Show Code") {
+                this.innerHTML = "Hide Code";
+                } else {
+                this.innerHTML = "Show Code";
+                }
+            });
+            }
