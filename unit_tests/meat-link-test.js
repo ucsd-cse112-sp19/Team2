e8832c0fa8deb453772366741a8e8ffb88e8a0a5
Veronica Lin <velin@ucsd.edu>
2019-06-02 15:55:08 -0700

    meat-link unit tests

--- unit_tests/meat-link-test.js
+++ unit_tests/meat-link-test.js
@@ -10,7 +10,7 @@ describe('meat-input basic requirements', function()  {
 
     it ("shadowDOM should exist", function(done) { 
         setTimeout(function() { 
-            sr = document.querySelector("meat-input").shadow; 
+            sr = comp.shadow; 
             assert.equal(sr instanceof ShadowRoot, true);
         }, 0)
         done();
@@ -28,29 +28,97 @@ describe("Text", function() {
         done()
     })
 })
+describe("color", async() => { 
+    it ("has color attribute", function(done) { 
+        comp.setAttribute("color", "green"); 
+        assert.equal(document.querySelector('meat-link').getAttribute("color"), "green");
+        done();
+    })
+    it ("correct css", function(done) { 
+        comp.setAttribute("color", "green"); 
+        const host = document.querySelector("meat-link");
+        const inner = host.shadow.querySelector("a");
+        const style = getComputedStyle(inner).color;
+        assert.equal(style, "rgb(89, 192, 64)");
+        done()
+    })
+
+})
+
 describe("href", function() { 
     it ("Should take in a href to meat-space.org"), function(done) { 
-        const comp = document.querySelector('meat-link')
-        comp.href = "www.meat-space.org";
         const test = document.querySelector('meat-link')
         assert.equal(test.href, "www.meat-space.org");
         done()
     }
 })
+
+describe("disabled", function() { 
+    it ("has disabled attribute", function(done) { 
+        comp.setAttribute("disabled", "true"); 
+        assert.equal(document.querySelector('meat-link').getAttribute("disabled"), "true");
+        done();
+    })
+    it ("correct css", function(done) { 
+        const host = document.querySelector("meat-link");
+        const inner = host.shadow.querySelector("a");
+        const style = getComputedStyle(inner).cursor;
+        assert.equal(style, "not-allowed");
+        done()
+    })
+
+})
+
+describe("underline", function() { 
+    it ("has underline attribute", function(done) { 
+        comp.setAttribute("underline", "never"); 
+        assert.equal(document.querySelector('meat-link').getAttribute("underline"), "never");
+        done();
+    })
+    it ("has underline never", function(done) { 
+        comp.setAttribute("underline", "never"); 
+        assert.equal(document.querySelector('meat-link').getAttribute("underline"), "never");
+        done();
+    })
+    it ("has underline hover", function(done) { 
+        comp.setAttribute("underline", "hover"); 
+        assert.equal(document.querySelector('meat-link').getAttribute("underline"), "hover");
+        done();
+    })
+    it ("has underline always", function(done) { 
+        comp.setAttribute("underline", "always"); 
+        assert.equal(document.querySelector('meat-link').getAttribute("underline"), "always");
+        done();
+    })
+})
+
 describe("bootstrap attribute", function() { 
-    /*
-    it ("Should have bootstrap nav-link", function(done) { 
+    it ("should have bootstrap nav-link", function(done) {
         comp.setAttribute("bootstrap", "nav-link");
+        assert.equal(document.querySelector('meat-link').getAttribute("bootstrap"), "nav-link");
+        done();
+    });
+    it ("correct css bootstrap nav-link", function(done) { 
         const host = document.querySelector("meat-link");
         const inner = host.shadow.querySelector("a");
         const hi = getComputedStyle(inner).display;
-        assert.equal(hi, block);
-        done()
-    })*/
-    /*
+        assert.equal(hi, "block");
+        done();
+    });
+    
     it ("Should have bootstrap navbar-brand", function(done) { 
         comp.setAttribute("bootstrap", "navbar-brand");
+        assert.equal(document.querySelector('meat-link').getAttribute("bootstrap"), "navbar-brand");
+        done();
     })
+    it ("correct css bootstrap navbar-brand", function(done) { 
+        const host = document.querySelector("meat-link");
+        const inner = host.shadow.querySelector("a");
+        const style = getComputedStyle(inner).display;
+        assert.equal(style, "inline-block");
+        done();
+    });
+    /*
     it ("Should have bootstrap nav-link dropdown-toggle", function(done) { 
         comp.setAttribute("bootstrap", "nav-link dropdown-toggle");
     })
@@ -59,24 +127,6 @@ describe("bootstrap attribute", function() {
 
     })*/
 });
-/*
-describe("color", function() { 
-    
-})
-describe("disabled", function() { 
-    
-})
-describe("href", function() { 
-    
-})
-describe("icon", function() { 
-    
-})
-describe("type", function() { 
-    
-})
-describe("underline", function() { 
-    
-})
-*/
+
+
 
