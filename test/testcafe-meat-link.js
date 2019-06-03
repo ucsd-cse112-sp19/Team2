88f07440c54f5b1ceff7957d581b8ac16358d591
Veronica Lin <velin@ucsd.edu>
2019-06-02 17:01:27 -0700

    add more testcafe meat-link test

--- test/testcafe-meat-link.js
+++ test/testcafe-meat-link.js
@@ -1,7 +1,7 @@
 import { Selector } from "testcafe"; // first import testcafe selectors
 
 fixture`Running tests for meat-link` // declare the fixture
-  .page`../web_components/meat-link/meat-link-demo.html`;
+   .page`../web_components/meat-link/meat-link-demo.html`;
 
 const getElementById = Selector(id => document.querySelector(id));
 
@@ -15,7 +15,22 @@ test("Click link", async t => {
   await t.click(component);
 });
 
+test("Correct CSS color", async t => {
+  const component = getElementById("#color");
+  await t
+        .expect(component.getStyleProperty('color')).eql('rgb(33, 37, 41)')
+});
+
+test("Cannot click disabled link", async t => {
+  const component = await Selector(() => document.querySelector('#disabled').shadowRoot.querySelector('a'));
+  await t
+        .hover(component)
+        .expect(component.getStyleProperty('cursor')).eql('pointer')
+});
+
 test("Hover link", async t => {
-  const component = getElementById("meat-link");
-  await t.hover(component);
+  const component = getElementById("#test");
+  await t
+        .hover(component)
+        .expect(component.getStyleProperty('text-decoration')).eql('none solid rgb(33, 37, 41)')
 });
