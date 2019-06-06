2b9b6e226600c3820da80a0299aec20e6aedcb21
Veronica Lin <velin@ucsd.edu>
2019-06-06 14:53:04 -0700

    allow access to shadowdom

--- test/testcafe-meat-button.js
+++ test/testcafe-meat-button.js
@@ -1,7 +1,7 @@
 import { Selector, ClientFunction } from "testcafe"; // first import testcafe selectors
 
 fixture`Running tests for meat-button` // declare the fixture
-  .page`../web_components/meat-button/meat-button-demo.html`;
+  .page `http://127.0.0.1:8080/web_components/meat-button/meat-button-demo.html`;
 
 const getElementById = Selector(id => document.querySelector(id));
 const setAttribute = ClientFunction((id, attributeName, value) => {
@@ -10,7 +10,8 @@ const setAttribute = ClientFunction((id, attributeName, value) => {
 
 test("Check that color attribute exists", async t => {
   const component = await getElementById("#check-button");
-  await t.expect(component.hasAttribute("color")).eql(true);
+  await t
+    .expect(component.hasAttribute("color")).eql(true);
 });
 test("Check white color attribute", async t => {
   const component = await getElementById("#check-button");
@@ -23,8 +24,13 @@ test("Check that tag-name is meat-button", async t => {
 });
 
 test("Click button", async t => {
-  const component = getElementById("meat-button");
-  await t.click(component);
+  const input = getElementById("#input");
+  const component = getElementById("#reset");
+  await t
+    .typeText(input, "meat-space")
+    .click(component)
+    .expect(input.textContent).eql('');
+    
 });
 
 test("Hover button", async t => {
--- test/testcafe-meat-input.js
+++ test/testcafe-meat-input.js
@@ -4,40 +4,56 @@ fixture`Running tests for meat-input` // declare the fixture
   .page`../web_components/meat-input/meat-input-demo.html`;
 
 const getElementById = Selector(id => document.querySelector(id));
-
-test("Check that the size attribute exists", async t => {
-  const component = await getElementById("meat-input");
-  await t.expect(component.getAttribute("size")).eql("small");
-});
-
-test("Allows user input", async t => { 
-  const component = await getElementById("meat-input");
-  await t
-        .typeText(component, "meat-space")
-        .expect(component.getAttribute("placeholder")).eql("meat-space")
-})
-
-test("Check that the placeholder attribute exists", async t => {
-  const component = await getElementById("meat-input");
-  await t.expect(component.getAttribute("placeholder")).eql("Small");
-});
+const getShadow = Selector(root => document.querySelector(root).shadowRoot); 
 
 test("Check that the autocomplete attribute exists", async t => {
   const component = await getElementById("meat-input");
   await t.expect(component.getAttribute("autocomplete")).eql("off");
+});
+test("Visuals of Default Button", async t => {
+  const component = await getElementById("meat-input");
+  const shadowInput = await Selector(() => document.querySelector('#basic').shadowRoot.querySelector('input'));
+  await t.expect(shadowInput.getStyleProperty('cursor')).eql("text");
+});
+
+test("Visuals of Disabled Button", async t => {
+  const component = await getElementById("#medium");
+  const shadowInput = await Selector(() => document.querySelector('#medium').shadowRoot.querySelector('input'));
+  await t
+    .hover(component)
+    .expect(shadowInput.getStyleProperty('width')).eql("159px");
 });
 
 test("Check that tag-name is meat-input", async t => {
   const component = getElementById("meat-input");
   await t.expect(component.tagName).eql("meat-input");
 });
-
+/*
 test("Click button", async t => {
   const component = getElementById("meat-input");
-  await t.click(component);
+  const shadowInput = await Selector(() => document.querySelector('#basic').shadowRoot.querySelector('#input'));
+  await t
+    .click(shadowInput)
+    .expect(shadowInput.getStyleProperty('border')).eql('');
 });
-
+*/
 test("Hover button", async t => {
   const component = getElementById("meat-input");
   await t.hover(component);
 });
+
+test("Typing text into input-field", async t => { 
+  const shadowInput = await Selector(() => document.querySelector('#basic').shadowRoot.querySelector('#input'));
+
+  await t
+    .typeText(shadowInput, "meat-space")
+    .expect(shadowInput.value).eql("Stuff inside tag should go inside inputmeat-space");
+})
+
+test("Cannot type in disabled input-field", async t => { 
+  const shadowInput = await Selector(() => document.querySelector('#disabledtest').shadowRoot.querySelector('#input'));
+
+  await t
+    .typeText(shadowInput, "meat-space")
+    .expect(shadowInput.value).eql("");
+})
